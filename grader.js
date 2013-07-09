22542966b58243303f34c576d556b67831bd5109
Tosin Onafowokan <toast4321@gmail.com>
2013-07-09 21:19:01 +0000

    Finished files

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "does not exist";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,29 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertHtmlExists = function(infile){
+    // variable for where the temp html will be written to
+    var htmlFile = "test.html";
+    var outJsonFile = "output.json";
+
+    rest.get(infile).on('complete',function(result){
+	fs.writeFileSync(htmlFile,result);
+
+	//check to make sure the file exists
+	if(!fs.existsSync(htmlFile)){
+	    console.log("%s does not exist. Existing.",htmlFile);
+	    process.exit(1);
+	}
+
+	var checkJson = checkHtmlFile(htmlFile, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+
+	//only including for our web project, not for file
+	fs.writeFileSync(outJsonFile,outJson);
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,10 +90,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_location>', 'Path to url', clone(assertHtmlExists), URLFILE_DEFAULT) 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    //if we do not have a web url fall back on the file
+    if (program.url == URLFILE_DEFAULT){
+	console.log("Using local file...");
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    } else {
+	console.log("Using webpage...");
+    }
+	
+    // commenting out because we call it above
+    //var checkJson = checkHtmlFile(inputTask, program.checks);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
