100ce703cfff3dca94de125085434c8af63560fb
Tosin Onafowokan <toast4321@gmail.com>
2013-07-09 02:19:33 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var buf = new Buffer();
 
 var app = express.createServer(express.logger());
 
+buf = fs.readFileSync('index.html',function(err,data){
+    if(err) throw err;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
